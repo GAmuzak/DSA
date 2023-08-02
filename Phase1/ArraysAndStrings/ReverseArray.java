9ffc7d8f4677e22ed6aa003682bc17b33e75c433
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-03 02:45:25 +0530

    ReverseArray

---
+++ Phase1/ArraysAndStrings/ReverseArray.java
@@ -0,0 +1,19 @@
+package Phase1.ArraysAndStrings;
+
+public class ReverseArray {
+    private static int[] reverse(int[] arr) {
+        int[] revArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            revArr[arr.length-1-i] = arr[i];
+        }
+        return revArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5};
+        int[] revArr = reverse(arr);
+        for (int i : revArr) {
+            System.out.print(i + ", " );
+        }
+    }
+}
