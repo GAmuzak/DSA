31b71235aa46b7f9cc4aea7453c2e38c88b07b6b
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-03 02:44:50 +0530

    Array Average

---
+++ Phase1/ArraysAndStrings/ArrayAvg.java
@@ -0,0 +1,20 @@
+package Phase1.ArraysAndStrings;
+
+public class ArrayAvg {
+
+    private static double getAvg(int[] arr) {
+        double sum = 0;
+        for (int i : arr) {
+            sum+=i;
+        }
+        double avg = sum/arr.length;
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5};
+        double sol = getAvg(arr);
+        System.out.println(sol);
+    }
+    
+}
