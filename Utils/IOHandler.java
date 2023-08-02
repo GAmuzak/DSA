7eedc1692268e45caae89b895fa1e669e6bcfbb9
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-03 02:45:47 +0530

    Io Handler

---
+++ Utils/IOHandler.java
@@ -0,0 +1,30 @@
+package Utils;
+
+import java.util.Scanner;
+
+public class IOHandler {
+    @SuppressWarnings("resource")
+    public static int[] handleArrayInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextLine(); //skip array length
+
+        String array = scanner.nextLine();
+        String[] splitVals = array.split(" ");
+        int[] arr = new int[splitVals.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(splitVals[i]);
+        }
+
+        return arr;
+    }
+
+    public static void printArray(int[] arr){
+        // System.out.print("\"");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if(i!=arr.length-1) System.out.print(" ");
+        }
+        // System.out.print("\"");
+    }
+}
