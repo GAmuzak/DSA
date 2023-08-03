bbfcf5d4d76b02fb44a58f5b1285fea2bb25b937
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-03 14:08:07 +0530

    Non Decreasing Array

---
+++ Phase1/ArraysAndStrings/NonDecreasing.java
@@ -0,0 +1,28 @@
+package Phase1.ArraysAndStrings;
+
+public class NonDecreasing {
+
+    public static boolean isPossible(int[] arr) {
+		boolean changed = false;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]<=arr[i+1]) continue;
+            else {
+                if(changed) return false;
+                if(i==0 || arr[i+1]>=arr[i-1]){
+                    arr[i]=arr[i+1];
+                }
+                else {
+                    arr[i+1]=arr[i];
+                }
+                changed = true;
+            }
+        }
+        return true;
+	}
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,1,2};
+        boolean result = isPossible(arr);
+        System.out.println(result);
+    }
+}
