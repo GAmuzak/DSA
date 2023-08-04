d2b241f74a331fe4bd9061a1da8d75497430d538
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-04 15:57:43 +0530

    So far so good

--- Phase1/ArraysAndStrings/Strings/NextPalindrome.java
+++ Phase1/ArraysAndStrings/Strings/NextPalindrome.java
@@ -2,43 +2,28 @@ package Phase1.ArraysAndStrings.Strings;
 
 public class NextPalindrome {
 
-    public static String generatePalindromeRight(StringBuilder numLeft, int isEven){
-        StringBuilder palin = new StringBuilder(numLeft);
-        for (int i = numLeft.length()-1 -isEven; i >=0; i--) {
-            palin.append(numLeft.charAt(i));
+    public static String nextLargestPalindrome(String number, int length) {
+        boolean isOddLength = length%2==1;
+        StringBuilder left = new StringBuilder();
+        String leftPart = number.substring(0, (length/2));
+        left.append(leftPart);
+        StringBuilder palin = new StringBuilder(left);
+        String center = "";
+        if(isOddLength){
+            center = number.substring((length/2), (length/2)+1);
+            palin.append(center);
         }
-        return palin.toString();
-    }
+        String right = left.reverse().toString();
+        palin.append(right);
 
-    public static String nextLargestPalindrome(String number) {
-		int cutoffLength = 0;
-        int isEven = number.length()%2;
-        if(isEven==0) cutoffLength = number.length()/2;
-        else cutoffLength = number.length()/2 +1;
-        String lp = number.substring(0, cutoffLength);
-        StringBuilder leftPart = new StringBuilder(lp);
-        String newNum = generatePalindromeRight(leftPart, isEven);
-        while(Integer.parseInt(number)>Integer.parseInt(newNum)){
-            int lastIndex = leftPart.length()-1;
-            if(leftPart.charAt(lastIndex)=='9'){
-                leftPart.setCharAt(lastIndex, (char)('0'));
-                int secondLast = leftPart.charAt(lastIndex-1)-'0';
-                secondLast++;
-                leftPart.setCharAt(lastIndex-1, (char)(secondLast+'0'));
-            }
-            else{
-                int lastDigit = leftPart.charAt(lastIndex) - '0';
-                lastDigit++;
-                leftPart.setCharAt(lastIndex, (char)(lastDigit + '0'));
-            }
-            newNum = generatePalindromeRight(leftPart, isEven);
-        }
-        return newNum;
+        
+
+        return "";
 	}
 
     public static void main(String[] args) {
-        String inp = "24956";
-        String op = nextLargestPalindrome(inp);
+        String inp = "24395";
+        String op = nextLargestPalindrome(inp, inp.length());
         System.out.println(op);
     }
 }
