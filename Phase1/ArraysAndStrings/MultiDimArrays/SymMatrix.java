57f8dc98de26fa0003f95f46c0ecc63d3102ce1e
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-05 23:29:42 +0530

    Symmetric Matrix

---
+++ Phase1/ArraysAndStrings/MultiDimArrays/SymMatrix.java
@@ -0,0 +1,23 @@
+package Phase1.ArraysAndStrings.MultiDimArrays;
+
+public class SymMatrix {
+    public static boolean isMatrixSymmetric(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix.length; j++) {
+                if(matrix[i][j]!=matrix[j][i]) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] twoDArray = {
+            {1,0},
+            {1,1}
+        };
+        // 1 2 3 4 5 6 7 8 9
+        boolean ans = isMatrixSymmetric(twoDArray);
+        System.out.println(ans);
+    }
+}
