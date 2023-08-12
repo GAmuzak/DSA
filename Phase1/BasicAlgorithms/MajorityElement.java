661f2d3f954b6fb65f73e4b416f05173ef9db548
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-13 00:16:01 +0530

    Moore's Algorithm

---
+++ Phase1/BasicAlgorithms/MajorityElement.java
@@ -0,0 +1,40 @@
+package Phase1.BasicAlgorithms;
+
+import Utils.IOHandler;
+import Utils.PrimitiveObjectConverter;
+
+public class MajorityElement {
+    public static int findMajority(int[] arr, int n) {
+        int maj = 0;
+        int majCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (maj == arr[i])
+                majCount++;
+            else {
+                if (majCount == 0) {
+                    maj = arr[i];
+                    majCount++;
+                } else {
+                    majCount--;
+                }
+            }
+        }
+        majCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == maj)
+                majCount++;
+        }
+        if (majCount > n / 2)
+            return maj;
+        else
+            return -1;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arrInp = IOHandler.handleArrayInput(Integer.class);
+        int[] arr = PrimitiveObjectConverter.convertToPrimitiveArray(arrInp);
+        int sol = findMajority(arr, arr.length);
+        System.out.println(sol);
+    }
+
+}
