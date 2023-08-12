2fc722090eb573670ab9f6988f065980c9b591fd
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-13 00:07:46 +0530

    Dutch National Flag

---
+++ Phase1/BasicAlgorithms/DutchNationalFlag.java
@@ -0,0 +1,43 @@
+package Phase1.BasicAlgorithms;
+
+import Utils.IOHandler;
+import Utils.PrimitiveObjectConverter;
+
+public class DutchNationalFlag {
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j)
+            return;
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    public static void sort012(int[] arr) {
+        int n = arr.length;
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Integer[] arrInp = IOHandler.handleArrayInput(Integer.class);
+        int[] arr = PrimitiveObjectConverter.convertToPrimitiveArray(arrInp);
+        sort012(arr);
+        Integer[] op = PrimitiveObjectConverter.ConvertToObjectArray(arr);
+        IOHandler.printArray(op);
+    }
+}
