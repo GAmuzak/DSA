5080665b6d94e7524a8be6a9b931a5b477bf8d65
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-12 22:35:21 +0530

    Bubble Sort

---
+++ Phase1/BasicAlgorithms/BubbleSort.java
@@ -0,0 +1,34 @@
+package Phase1.BasicAlgorithms;
+
+import Utils.IOHandler;
+import Utils.PrimitiveObjectConverter;
+
+public class BubbleSort {
+
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    public static void bubbleSort(int[] arr, int n) {
+        boolean allSorted = false;
+        while (!allSorted) {
+            allSorted = true;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i - 1]) {
+                    swap(arr, i, i - 1);
+                    allSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arrInp = IOHandler.handleArrayInput(Integer.class);
+        int[] arr = PrimitiveObjectConverter.convertToPrimitiveArray(arrInp);
+        bubbleSort(arr, arr.length);
+        Integer[] op = PrimitiveObjectConverter.ConvertToObjectArray(arr);
+        IOHandler.printArray(op);
+    }
+}
