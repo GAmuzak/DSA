c73ed003a8e9fe90781a13a7208518bc2293eea1
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-12 10:02:04 +0530

    Pair Sum Redone

--- Phase1/TwoPointers/PairSum.java
+++ Phase1/TwoPointers/PairSum.java
@@ -1,35 +1,39 @@
 package Phase1.TwoPointers;
 
+import java.util.Scanner;
+
 import Utils.IOHandler;
 import Utils.PrimitiveObjectConverter;
 
 public class PairSum {
 
     public static int pairSum(int arr[], int n, int target) {
-        int i=0;
-        int j=n-1;
-        int count=0;
-        while (i<j) {
-            int sum = arr[i]+arr[j];
-            if (sum < target)
-                i++;
-            else if (sum > target) 
-                j--;
-            else{
-                count++;
-                i++;
-                j--;
+        int left = 0, right = n - 1;
+        int pairCount = 0, sum = 0;
+        while (left < right) {
+            sum = arr[left] + arr[right];
+            if (sum == target) {
+                pairCount++;
+                left++;
+                right--;
+            }
+            if (target > sum) {
+                right--;
+            }
+            if (target < sum) {
+                left++;
             }
         }
-        if(count == 0)
-        count = -1;
-        return count;
+        return pairCount>0?pairCount:-1;
     }
 
     public static void main(String[] args) {
         Integer[] arr = IOHandler.handleArrayInput(Integer.class);
         int[] arrInts = PrimitiveObjectConverter.convertToPrimitiveArray(arr);
-        int sol = pairSum(arrInts, arrInts.length, 12);
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        sc.close();
+        int sol = pairSum(arrInts, arrInts.length, target);
         System.out.println(sol);
     }
 }
