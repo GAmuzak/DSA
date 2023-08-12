627bf1570ca016d964d0fe4f385ae0b386a2b6c3
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-12 10:25:13 +0530

    Separate Negative and Positive

---
+++ Phase1/TwoPointers/NegPosSeparation.java
@@ -0,0 +1,40 @@
+package Phase1.TwoPointers;
+
+import Utils.IOHandler;
+import Utils.PrimitiveObjectConverter;
+
+public class NegPosSeparation {
+
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    public static int[] separateNegativeAndPositive(int a[]) {
+        int left = 0, right = a.length - 1;
+        while (left < right) {
+            if (a[left] < 0) {
+                //already negative
+                left++;
+            } else if (a[right] >= 0) {
+                //already positive
+                right--;
+            } else {
+                swap(a, left, right);
+                left++;
+                right--;
+            }
+        }
+        return a;
+    }
+    
+    public static void main(String[] args) {
+        Integer[] arrInp = IOHandler.handleArrayInput(Integer.class);
+        int arr[] = PrimitiveObjectConverter.convertToPrimitiveArray(arrInp);
+        int[] newArr = separateNegativeAndPositive(arr);
+        Integer[] sol = PrimitiveObjectConverter.ConvertToObjectArray(newArr);
+        IOHandler.printArray(sol);
+    }
+
+}
