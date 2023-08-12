2a5f0c624fabfaded9f1850737bff04a997f6028
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-12 19:53:14 +0530

    Insertion Sort

---
+++ Phase1/BasicAlgorithms/InsertionSort.java
@@ -0,0 +1,31 @@
+package Phase1.BasicAlgorithms;
+
+import Utils.IOHandler;
+import Utils.PrimitiveObjectConverter;
+
+public class InsertionSort {
+
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    public static void insertionSort(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j>0 && arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arrInp = IOHandler.handleArrayInput(Integer.class);
+        int[] arr = PrimitiveObjectConverter.convertToPrimitiveArray(arrInp);
+        insertionSort(arr.length, arr);
+        Integer[] op = PrimitiveObjectConverter.ConvertToObjectArray(arr);
+        IOHandler.printArray(op);
+    }
+}
