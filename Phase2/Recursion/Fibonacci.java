6355e8114a341be2f9e788b573b385a29c21d0c1
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-20 13:29:11 +0530

    With Memoisation

--- Phase2/Recursion/Fibonacci.java
+++ Phase2/Recursion/Fibonacci.java
@@ -1,16 +1,25 @@
 package Phase2.Recursion;
 
+import java.util.HashMap;
+
 public class Fibonacci {
-    public static long fib(long n) {
-        if (n > 2) {
-            return fib(n - 2) + fib(n - 1);
-        } else {
+    public static long fib(long n, HashMap<Long, Long> map) {
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        if (n <= 2) {
             return 1;
         }
+        long result = fib(n - 1, map) + fib(n - 2, map);
+        map.put(n, result);
+        return result;
     }
-
+    public static long fibMain(long n) {
+        HashMap<Long, Long> memMap = new HashMap<>();
+        return fib(n, memMap);
+    }
     public static void main(String[] args) {
-        long sol = fib(9);
+        long sol = fibMain(9);
         System.out.println(sol);
     }
 }
