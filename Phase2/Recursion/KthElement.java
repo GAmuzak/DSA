4a6bd7a955eb2184ef7e5291459050ffe1b5eadd
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-24 21:05:25 +0530

    Mild Optimisation

--- Phase2/Recursion/KthElement.java
+++ Phase2/Recursion/KthElement.java
@@ -11,16 +11,12 @@ public class KthElement {
         k--;
         while (k >= 0) {
             if (ctr1 >= arr1.size()) {
-                target = arr2.get(ctr2);
-                ctr2++;
-                k--;
-                continue;
+                target = arr2.get(ctr2 + k);
+                break;
             }
             if (ctr2 >= arr2.size()) {
-                target = arr1.get(ctr1);
-                ctr1++;
-                k--;
-                continue;
+                target = arr1.get(ctr1 + k);
+                break;
             }
             if (arr1.get(ctr1) < arr2.get(ctr2)) {
                 target = arr1.get(ctr1);
