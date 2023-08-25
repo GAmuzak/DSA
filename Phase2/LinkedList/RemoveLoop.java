3964ccb6125d648b6a95794d28f809f6c4d3f086
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-25 15:27:53 +0530

    Remove Loop

--- Phase2/LinkedList/RemoveLoop.java
+++ Phase2/LinkedList/RemoveLoop.java
@@ -2,6 +2,27 @@ package Phase2.LinkedList;
 
 public class RemoveLoop {
     public static Node removeLoop(Node head) {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return head;
+        }
+        Node fromStart = head;
+        while (fromStart != slow) {
+            fromStart = fromStart.next;
+            slow = slow.next;
+        }
+        Node prev = slow;
+        while (prev.next != slow) {
+            prev = prev.next;
+        }
+        prev.next = null;
         return head;
     }
 }
@@ -9,6 +30,7 @@ public class RemoveLoop {
 class Node {
     int data;
     Node next;
+
     Node(int data) {
         this.data = data;
         this.next = null;
