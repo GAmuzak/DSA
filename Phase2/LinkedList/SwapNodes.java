00746ffdd2280ead8301e17a9659f8065ef91237
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-25 20:36:21 +0530

    Swap Nodes

---
+++ Phase2/LinkedList/SwapNodes.java
@@ -0,0 +1,32 @@
+package Phase2.LinkedList;
+
+public class SwapNodes {
+    public static Node pairsSwap(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node base = new Node(0);
+        base.next = head;
+        Node prev = base;
+        while (prev.next != null && prev.next.next != null) {
+            Node current = prev.next;
+            Node nextNode = current.next;
+            current.next = nextNode.next;
+            nextNode.next = current;
+            prev.next = nextNode;
+
+            prev = current;
+        }
+        return base.next;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
