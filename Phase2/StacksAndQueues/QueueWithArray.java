721f0720111dcf56583b446eb7adb24ba275ce1c
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-27 12:40:55 +0530

    Queue With Array

---
+++ Phase2/StacksAndQueues/QueueWithArray.java
@@ -0,0 +1,63 @@
+package Phase2.StacksAndQueues;
+
+public class QueueWithArray {
+    class Queue {
+
+        int size;
+        Node head;
+        Node tail;
+
+        Queue() {
+            size = 0;
+            head = null;
+            tail = head;
+        }
+
+        boolean isEmpty() {
+            return size == 0;
+        }
+
+        void enqueue(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = head;
+            } else {
+                tail.next = newNode;
+                tail = tail.next;
+            }
+            size++;
+        }
+
+        int dequeue() {
+            if (head == null)
+                return -1;
+            int num = head.data;
+            head = head.next;
+            size--;
+            return num;
+        }
+
+        int front() {
+            if (head == null)
+                return -1;
+            return head.data;
+        }
+    }
+
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
