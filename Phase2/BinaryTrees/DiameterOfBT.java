a4ea8f652c4d6caa443828aa2bc6dbcf301443ff
Rishav Banerjee <thatamuzak@gmail.com>
2023-09-02 10:46:03 +0530

    Vertical Order Traversal

--- Phase2/BinaryTrees/DiameterOfBT.java
+++ Phase2/BinaryTrees/DiameterOfBT.java
@@ -1,38 +1,48 @@
 package Phase2.BinaryTrees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DiameterOfBT {
 
-    public static int findDiam(TreeNode node, int[] sol) {
-        if (node == null)
-            return -1;
-        int left = findDiam(node.left, sol);
-        int right = findDiam(node.right, sol);
-        sol[0] = Math.max(sol[0], left + right + 2);
-        return 1 + Math.max(left, right);
-    }
-
-    public static int diameterOfBinaryTree(TreeNode root) {
-        int[] sol = new int[] { Integer.MIN_VALUE };
-        findDiam(root, sol);
-        return sol[0];
+    public static int diameterOfBinaryTree(TreeNode<Integer> root) {
+        if (root == null)
+            return 0;
+        int maxDiameter = 0;
+        Queue<TreeNode<Integer>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode<Integer> current = queue.poll();
+            int leftHeight = 0;
+            int rightHeight = 0;
+            TreeNode<Integer> left = current.left;
+            TreeNode<Integer> right = current.right;
+            while (left != null) {
+                leftHeight++;
+                left = left.left;
+            }
+            while (right != null) {
+                rightHeight++;
+                right = right.right;
+            }
+            maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
+            if (current.left != null)
+                queue.offer(current.left);
+            if (current.right != null)
+                queue.offer(current.right);
+        }
+        return maxDiameter;
     }
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+class TreeNode<T> {
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
---
+++ Phase2/BinaryTrees/VerticalOrderTraversal.java
@@ -0,0 +1,64 @@
+package Phase2.BinaryTrees;
+
+import java.util.*;
+
+public class VerticalOrderTraversal {
+
+    public static void addToMap(TreeMap<Integer, ArrayList<Integer>> columnTrack, int layer, int data) {
+        if (columnTrack.containsKey(layer)) {
+            columnTrack.get(layer).add(data);
+        } else {
+            ArrayList<Integer> newList = new ArrayList<>();
+            newList.add(data);
+            columnTrack.put(layer, newList);
+        }
+    }
+
+    public static ArrayList<Integer> verticalOrderTraversal(TreeNode<Integer> root) {
+        ArrayList<Integer> sol = new ArrayList<>();
+        TreeMap<Integer, ArrayList<Integer>> columnTrack = new TreeMap<>();
+        Queue<NodeLayerPair> pairs = new LinkedList<>();
+        pairs.add(new NodeLayerPair(0, root));
+        while (!pairs.isEmpty()) {
+            int size = pairs.size();
+            ArrayList<NodeLayerPair> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                NodeLayerPair currPair = pairs.poll();
+                addToMap(columnTrack, currPair.layer, currPair.node.data);
+                if (currPair.node.left != null) {
+                    level.add(new NodeLayerPair(currPair.layer - 1, currPair.node.left));
+                }
+                if (currPair.node.right != null) {
+                    level.add(new NodeLayerPair(currPair.layer + 1, currPair.node.right));
+                }
+            }
+            pairs.addAll(level);
+        }
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : columnTrack.entrySet()) {
+            sol.addAll(entry.getValue());
+        }
+        return sol;
+    }
+}
+
+class NodeLayerPair {
+    int layer;
+    TreeNode<Integer> node;
+
+    public NodeLayerPair(int layer, TreeNode<Integer> node) {
+        this.layer = layer;
+        this.node = node;
+    }
+}
+
+class TreeNode<T> {
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    public TreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
