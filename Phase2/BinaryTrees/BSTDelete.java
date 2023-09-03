587ed646d9f33d52454c798d46f8dc6c539e53b3
Rishav Banerjee <thatamuzak@gmail.com>
2023-09-03 20:46:34 +0530

    Delete Node in BST

---
+++ Phase2/BinaryTrees/BSTDelete.java
@@ -0,0 +1,39 @@
+package Phase2.BinaryTrees;
+
+public class BSTDelete {
+
+    public static BinaryTreeNode<Integer> bstDelete(BinaryTreeNode<Integer> root, int key) {
+        if (root == null)
+            return root;
+        if (root.data > key)
+            root.left = bstDelete(root.left, key);
+        else if (root.data < key)
+            root.right = bstDelete(root.right, key);
+        else {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            else {
+                BinaryTreeNode<Integer> currNode = root.right;
+                while (currNode.left != null)
+                    currNode = currNode.left;
+                root.data = currNode.data;
+                root.right = bstDelete(root.right, currNode.data);
+            }
+        }
+        return root;
+    }
+}
+
+class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
--- Phase2/BinaryTrees/LCAofBST.java
+++ Phase2/BinaryTrees/LCAofBST.java
@@ -16,3 +16,27 @@ public class LCAofBST {
         return null;
     }
 }
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+        this.data = 0;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+};
